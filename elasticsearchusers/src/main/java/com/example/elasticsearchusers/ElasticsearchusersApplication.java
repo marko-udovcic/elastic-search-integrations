224252354a3fa6af7aa626269ae667947e9ca9d6
marko udovcic <markoudovcic53@gmail.com>
2025-03-21 12:33:35 +0100

    add all configuration files

---
+++ elasticsearchusers/src/main/java/com/example/elasticsearchusers/ElasticsearchusersApplication.java
@@ -0,0 +1,15 @@
+package com.example.elasticsearchusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchusersApplication.class, args);
+	}
+
+
+
+}
